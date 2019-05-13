d760a6e09e5fbbb10c300721ac3f8d8a110d22c3
jufabeck2202 <ju-fa-beck@t-online.de>
2019-05-13 23:00:03 +0200

    der dem dieses repo gehört ist komisch

---
+++ Assets/rotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    public float rotationSpeedX = 0;
+    public float rotationSpeedY = 0;
+    public float rotationSpeedZ = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.Rotate(Vector3.one * rotationSpeed * Time.deltaTime);
+        transform.Rotate(rotationSpeedX * Time.deltaTime, rotationSpeedY* Time.deltaTime, rotationSpeedZ * Time.deltaTime);
+        
+    }
+}
