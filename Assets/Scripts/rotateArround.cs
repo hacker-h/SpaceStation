93221da73da1f425acfba8899c8bfa2918018654
Julian Beck <ju-fa-beck@t-online.de>
2019-05-20 13:42:19 +0200

    added rotation script

--- Assets/Scripts/rotateArround.cs
+++ Assets/Scripts/rotateArround.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class rotateArround : MonoBehaviour
 {
+    public int RotationSpeed = 100;
     public GameObject Earth;
     // Start is called before the first frame update
     void Start()
@@ -13,7 +14,7 @@ public class rotateArround : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(Earth.transform.position, Earth.transform.up, 100 * Time.deltaTime);
+        transform.RotateAround(Earth.transform.position, Earth.transform.up, RotationSpeed * Time.deltaTime);
 
     }
 }
