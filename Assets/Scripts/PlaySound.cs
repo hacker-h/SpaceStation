5863ee0652c9603dc38cd3c1ffbccbcefcba5537
jufabeck2202 <ju-fa-beck@t-online.de>
2019-06-24 09:23:11 +0200

    added suma

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public AudioSource tickSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tickSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        tickSource.Play();
+    }
+}
