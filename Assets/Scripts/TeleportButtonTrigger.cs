04556247b2a2490188ad9c0f999b308634117d9d
jufabeck2202 <ju-fa-beck@t-online.de>
2019-07-20 09:51:47 +0200

    added build

---
+++ Assets/Scripts/TeleportButtonTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportButtonTrigger : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform Player;
+    [SerializeField]
+    private Transform spawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        Player.position = spawnPoint.position;
+
+    }
+}
